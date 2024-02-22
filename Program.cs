9a3c4e8fa92ba6812933b530abce36bcc0824a3c
Radmir Nikolaev <radmir.nikolaev.04@mail.ru>
2024-02-22 14:59:06 +0300

    update code and add count

--- Program.cs
+++ Program.cs
@@ -7,14 +7,28 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-System.Console.Write("Введите длину массива: ");
-int arrLen = int.Parse(Console.ReadLine());
-
-string[] sArr = new string[arrLen];
-
-System.Console.WriteLine("Заполните массив значениями");
-for (int i = 0; i < arrLen; i++)
+string[] CreateArr()
 {
-    System.Console.Write($"{i + 1}: ");
-    sArr[i] = Console.ReadLine();
+    System.Console.Write("Введите длину массива: ");
+    int arrLen = int.Parse(Console.ReadLine());
+
+    string[] sArr = new string[arrLen];
+
+    System.Console.WriteLine("Заполните массив значениями");
+    for (int i = 0; i < arrLen; i++)
+    {
+        System.Console.Write($"{i + 1}: ");
+        sArr[i] = Console.ReadLine();
+    }
+    return sArr;
 }
+
+int CountingShortValue(string[] arr, int lenght)
+{
+    int k = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i].Length <= lenght)
+            k++;
+    return k;
+}
+
