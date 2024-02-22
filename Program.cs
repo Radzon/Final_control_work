a0b737db073cbfe2944b4a002bf3acb64e062b4b
Radmir Nikolaev <radmir.nikolaev.04@mail.ru>
2024-02-22 14:35:42 +0300

    add data imput

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+System.Console.Write("Введите длину массива: ");
+int arrLen = int.Parse(Console.ReadLine());
+
+string[] sArr = new string[arrLen];
+
+System.Console.WriteLine("Заполните массив значениями");
+for (int i = 0; i < arrLen; i++)
+{
+    System.Console.Write($"{i + 1}: ");
+    sArr[i] = Console.ReadLine();
+}
